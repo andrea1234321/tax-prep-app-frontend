9b65bb4baf631f0a73b0f3f28229747486f634dc
Andrea <andrearewards@hotmail.com>
2024-04-29 15:22:49 -0400

    add filing status to financial information page, crate N/a checkboxes for optional information

--- src/pages/FinanceInfo.tsx
+++ src/pages/FinanceInfo.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import ProgressBar from "../components/ProgressBar";
-import { Fieldset, RequiredMarker, Label, TextInput, Form, Button } from "@trussworks/react-uswds";
+import { Fieldset, RequiredMarker, Label, TextInput, Form, Button, Radio, Checkbox, Grid, InputGroup, InputPrefix } from "@trussworks/react-uswds";
 
 const FinanceInfo = () => {
+    const [otherIncome, setOtherIncome] = useState(false)
+    const [businessWithheld, setBusinessWithheld] = useState(false)
+    const [otherWithheld, setOtherWithheld] = useState(false)
+    const [paidWithheld, setPaidWithheld] = useState(false)
+
     return (
         <>
             <main id="main-content">
@@ -11,18 +17,73 @@ const FinanceInfo = () => {
                     <p>
                         Required fields are marked with an asterisk (<RequiredMarker />).
                     </p>
+                    <Label htmlFor="filling-status" requiredMarker>Filling Status: </Label>
+                    <Grid row>
+                        <Grid col={3} offset={1}>
+                            <Radio id="single" name="filing-status" label="Single" />
+                        </Grid>
+                        <Grid col={4}>
+                            <Radio id="married-jointly" name="filing-status" label="Married Filling Jointly" />
+                        </Grid>
+                        <Grid col={4}>
+                            <Radio id="married-separately" name="filing-status" label="Married Filling Separately" />      
+                        </Grid>
+                    </Grid>
                     <Label htmlFor="w2-income" requiredMarker>W2 Form(s) total income</Label>
                     <TextInput id="w2-income" name="w2-income" type="text" required/>
-                    <Label htmlFor="other-income" requiredMarker>Other income</Label>
-                    <TextInput id="other-income" name="other-income" type="text" required/>
+                    <Label htmlFor="other-income" >Other income</Label>
+                    <Grid row>
+                        <Grid col={6}>
+                            <InputGroup>
+                                <InputPrefix>$</InputPrefix>
+                                <TextInput id="other-income" name="other-income" type="number" disabled={otherIncome}/>
+                            </InputGroup>
+                        </Grid>
+                        <Grid col={4} offset={2}>
+                            <Checkbox id="na other-income" name="other-income" label="N/a" checked={otherIncome} onChange={() => setOtherIncome(!otherIncome)}/>
+                        </Grid>
+                    </Grid>
                     <Label htmlFor="w2-tax-withheld" requiredMarker>W2 Form(s) total income tax withheld</Label>
-                    <TextInput id="w2-tax-withheld" name="w2-tax-withheld" type="text" required/>
-                    <Label htmlFor="tax-withheld-1099" requiredMarker>1099 Form(s) total income tax withheld</Label>
-                    <TextInput id="tax-withheld-1099" name="tax-withheld-1099" type="text" required/>
-                    <Label htmlFor="other-tax-withheld" requiredMarker>Other tax withheld</Label>
-                    <TextInput id="other-tax-withheld" name="other-tax-withheld" type="text" required/>
-                    <Label htmlFor="paid-taxes-withheld" requiredMarker>Paid taxes from 2022</Label>
-                    <TextInput id="paid-taxes-withheld" name="paid-taxes-withheld" type="text" required/>
+                    <InputGroup>
+                        <InputPrefix>$</InputPrefix>
+                        <TextInput id="w2-tax-withheld" name="w2-tax-withheld" type="number" required/>
+                    </InputGroup>
+                    <Label htmlFor="tax-withheld-1099" >1099 Form(s) total income tax withheld</Label>
+                    <Grid row>
+                        <Grid col={6}>
+                            <InputGroup>
+                                <InputPrefix>$</InputPrefix>
+                                <TextInput id="tax-withheld-1099" name="tax-withheld-1099" type="number" disabled={businessWithheld}/>
+                            </InputGroup>
+                        </Grid>
+                        <Grid col={4} offset={2}>
+                            <Checkbox id="na tax-withheld-1099" name="tax-withheld-1099" label="N/a" checked={businessWithheld} onChange={() => setBusinessWithheld(!businessWithheld)}/>
+                        </Grid>
+                    </Grid>
+                    <Label htmlFor="other-tax-withheld" >Other tax withheld</Label>
+                    <Grid row>
+                        <Grid col={6}>
+                            <InputGroup>
+                                <InputPrefix>$</InputPrefix>
+                                <TextInput id="other-tax-withheld" name="other-tax-withheld" type="number" disabled={otherWithheld}/>
+                            </InputGroup>
+                        </Grid>
+                        <Grid col={4} offset={2}>
+                            <Checkbox id="na other-tax-withheld" name="other-tax-withheld" label="N/a" checked={otherWithheld} onChange={() => setOtherWithheld(!otherWithheld)}/>
+                        </Grid>
+                    </Grid>
+                    <Label htmlFor="paid-taxes-withheld" >Paid taxes from 2022</Label>
+                    <Grid row>
+                        <Grid col={6}>
+                            <InputGroup>
+                                <InputPrefix>$</InputPrefix>
+                                <TextInput id="paid-taxes-withheld" name="paid-taxes-withheld" type="number" disabled={paidWithheld}/>
+                            </InputGroup>
+                        </Grid>
+                        <Grid col={4} offset={2}>
+                            <Checkbox id="na paid-taxes-withheld" name="paid-taxes-withheld" label="N/a" checked={paidWithheld} onChange={() => setPaidWithheld(!paidWithheld)}/>
+                        </Grid>
+                    </Grid>
                     <Button type="submit">Review</Button>
                     </Fieldset>
                 </Form>
